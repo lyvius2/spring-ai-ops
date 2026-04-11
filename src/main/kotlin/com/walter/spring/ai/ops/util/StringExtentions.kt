6fafbe58b05e572eab5653aa6478586247626972
walter.hwang <lyvius2@naver.com>
2026-04-12 01:40:30 +0900

    String -> LocalDateTime 확장함수

--- src/main/kotlin/com/walter/spring/ai/ops/util/StringExtentions.kt
+++ src/main/kotlin/com/walter/spring/ai/ops/util/StringExtentions.kt
@@ -3,9 +3,9 @@ package com.walter.spring.ai.ops.util
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
 
-fun LocalDateTime.fromISO8601(dateTimeString: String): LocalDateTime? {
+fun String.toISO8601(): LocalDateTime? {
     return try {
-        OffsetDateTime.parse(dateTimeString).toLocalDateTime()
+        OffsetDateTime.parse(this).toLocalDateTime()
     } catch (_: Exception) {
         null
     }
