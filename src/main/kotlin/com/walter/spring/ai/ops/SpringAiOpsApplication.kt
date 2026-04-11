c04a445604e6c800d5e843d064a4daa8932ae023
walter.hwang <lyvius2@naver.com>
2026-04-11 23:19:13 +0900

    Spring Cloud 의존성 추가

--- src/main/kotlin/com/walter/spring/ai/ops/SpringAiOpsApplication.kt
+++ src/main/kotlin/com/walter/spring/ai/ops/SpringAiOpsApplication.kt
@@ -2,8 +2,10 @@ package com.walter.spring.ai.ops
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class SpringAiOpsApplication
 
 fun main(args: Array<String>) {
