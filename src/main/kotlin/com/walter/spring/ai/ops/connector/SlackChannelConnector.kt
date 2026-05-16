62c4840281f9443b33f67bad7b75545638672853
walter.hwang <lyvius2@naver.com>
2026-05-16 18:21:00 +0900

    Slack Connector 생성

---
+++ src/main/kotlin/com/walter/spring/ai/ops/connector/SlackChannelConnector.kt
@@ -0,0 +1,26 @@
+package com.walter.spring.ai.ops.connector
+
+import com.walter.spring.ai.ops.connector.dto.SlackMessageRequest
+import com.walter.spring.ai.ops.connector.dto.SlackMessageResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
+
+@Component
+class SlackChannelConnector(
+    @Value("\${slack.url}")
+    private val slackUrl: String,
+) {
+    private val client = RestClient.create(slackUrl)
+
+    fun sendMessage(message: SlackMessageRequest, channelPath: String): SlackMessageResponse {
+        return client.post()
+            .uri(channelPath)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(message)
+            .retrieve()
+            .body<SlackMessageResponse>() ?: throw RuntimeException("Failed to send message to Slack")
+    }
+}
